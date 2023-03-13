428dfe4be9e4f80c37c76acf23fb83b33820f2d8
Cipy348 <cipriangabriel112@gmail.com>
2023-03-13 16:18:04 +0200

    deleted goto

--- main.cpp
+++ main.cpp
@@ -140,102 +140,103 @@ int main() {
     e[2].setprogramlang("Typescript");
     e[2].setexperience(5);
 
-    start:
-
-    std::cout<<"1 - Add employee"<<"\n";
-    std::cout<<"2 - Remove employee"<<"\n";
-    std::cout<<"3 - Change program language and level of experience"<<"\n";
-    std::cout<<"4 - Display the employees"<<"\n";
-
-    int n;
-    std::cin>>n;
-    std::cin.get();
-
-    switch(n)
+    int n = 1;
+    while(n == 1)
     {
-        case 1:
-        {
-            std::cout<<"Adding new employee"<<"\n";
-            nremployees++;
-            Employee *aux = new Employee[nremployees];
-            for(int i=0;i<nremployees-1;i++)
-                aux[i] = e[i];
-            std::cin>>aux[nremployees-1];
-            delete[] e;
-            e = aux;
-            break;
-        }
+        std::cout<<"1 - Add employee"<<"\n";
+        std::cout<<"2 - Remove employee"<<"\n";
+        std::cout<<"3 - Change program language and level of experience"<<"\n";
+        std::cout<<"4 - Display the employees"<<"\n";
 
-        case 2:
+        int q;
+        std::cin>>q;
+        std::cin.get();
+
+        switch(q)
         {
-            char namefired[100];
-            int k=0;
-            std::cout<<"The name of the dismissed employee:";
-            std::cin.getline(namefired, 100);
-            Employee *aux = new Employee[nremployees-1];
-            for(int i=0;i<nremployees;i++)
-                if(strcmp(namefired, e[i].getname())==0)
-                {
-                    k=1;
-                    break;
-                }
-            if(k==0)
+            case 1:
             {
-                std::cout<<"This employee doesn't exist"<<"\n"<<"\n"<<"\n";
+                std::cout<<"Adding new employee"<<"\n";
+                nremployees++;
+                Employee *aux = new Employee[nremployees];
+                for(int i=0;i<nremployees-1;i++)
+                    aux[i] = e[i];
+                std::cin>>aux[nremployees-1];
+                delete[] e;
+                e = aux;
                 break;
             }
-            int i=0;
-            for(int j=0;j<nremployees;j++)
-                if(strcmp(namefired, e[j].getname())!=0)
-                    aux[i] = e[j],i++;
-            delete[] e;
-            e = aux;
-            nremployees--;
-            break;
-        }
 
-        case 3:
-        {
-            char nameemployee[100], program[100];
-            short exp, k=0;
-            std::cout<<"Employee name:";
-            std::cin.getline(nameemployee, 100);
-            for(int i=0;i<nremployees;i++)
-                if(strcmp(nameemployee, e[i].getname())==0)
+            case 2:
+            {
+                char namefired[100];
+                int k=0;
+                std::cout<<"The name of the dismissed employee:";
+                std::cin.getline(namefired, 100);
+                Employee *aux = new Employee[nremployees-1];
+                for(int i=0;i<nremployees;i++)
+                    if(strcmp(namefired, e[i].getname())==0)
+                    {
+                        k=1;
+                        break;
+                    }
+                if(k==0)
                 {
-                    k=1;
+                    std::cout<<"This employee doesn't exist"<<"\n"<<"\n"<<"\n";
                     break;
                 }
-            if(k==0)
-            {
-                std::cout<<"This employee doesn't exist"<<"\n"<<"\n"<<"\n";
+                int i=0;
+                for(int j=0;j<nremployees;j++)
+                    if(strcmp(namefired, e[j].getname())!=0)
+                        aux[i] = e[j],i++;
+                delete[] e;
+                e = aux;
+                nremployees--;
                 break;
             }
-            std::cout<<"New program:";
-            std::cin.getline(program, 100);
-            std::cout<<"New level of experience:";
-            std::cin>>exp;
-            for(int i=0;i<nremployees;i++)
-                if(strcmp(nameemployee, e[i].getname())==0)
+
+            case 3:
+            {
+                char nameemployee[100], program[100];
+                short exp, k=0;
+                std::cout<<"Employee name:";
+                std::cin.getline(nameemployee, 100);
+                for(int i=0;i<nremployees;i++)
+                    if(strcmp(nameemployee, e[i].getname())==0)
+                    {
+                        k=1;
+                        break;
+                    }
+                if(k==0)
                 {
-                    e[i].setprogramlang(program);
-                    e[i].setexperience(exp);
+                    std::cout<<"This employee doesn't exist"<<"\n"<<"\n"<<"\n";
                     break;
                 }
-            break;
+                std::cout<<"New program:";
+                std::cin.getline(program, 100);
+                std::cout<<"New level of experience:";
+                std::cin>>exp;
+                for(int i=0;i<nremployees;i++)
+                    if(strcmp(nameemployee, e[i].getname())==0)
+                    {
+                        e[i].setprogramlang(program);
+                        e[i].setexperience(exp);
+                        break;
+                    }
+                break;
+            }
+            case 4:
+                for(int i=0; i<nremployees;i++)
+                    std::cout<<e[i]<<"\n";
+                std::cout<<"\n";
+                break;
         }
-        case 4:
-            for(int i=0; i<nremployees;i++)
-                std::cout<<e[i]<<"\n";
-            std::cout<<"\n";
-            break;
+        std::cout<<"0 - Close the program"<<"\n";
+        std::cout<<"1 - Go back to menu"<<"\n";
+
+        std::cin>>n;
     }
-    std::cout<<"0 - Close the program"<<"\n";
-    std::cout<<"1 - Go back to menu"<<"\n";
 
-    std::cin>>n;
-    if(n==1)
-        goto start;
     delete[] e;
     return 0;
 }
